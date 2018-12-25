b61fec3505357bf32286bc71d00b41316b1418ac
weeeek <407351071@qq.com>
2018-12-25 19:04:55 +0800

    选择加载

--- src/components/raise/raise-list.vue
+++ src/components/raise/raise-list.vue
@@ -48,7 +48,7 @@ export default {
   data () {
     return {
       show:true,
-      all:true,
+      all:false,
       blossom:true,
       YYHList:[],
       modianDatas: []
@@ -57,8 +57,11 @@ export default {
   watch:{
     all(newValue){
       this.all = newValue
+      console.log(newValue)
+
       this.YYHList.map((item)=>{
         item.checked = this.all
+        this.exchange(item)
       })
     }
   },
@@ -66,7 +69,7 @@ export default {
     let _this = this;
     // 1、创建访问数据库的对象
     // 2、使用事务处理
-    var db = openDatabase("Mydata", "", "My Database", 1024 * 100);
+    var db = openDatabase("TeamSII", "", "Team SII Database", 1024 * 100);
     /*
         openDatabase(),作用打开数据，如果数据库不存在，则会创建一个新的数据库。
         五个参数说明：数据库名称、版本号、描述文本、数据库大小、创建回调
@@ -74,14 +77,16 @@ export default {
     //事件处理，异步
     db.transaction(function(tx) {
         tx.executeSql("CREATE TABLE IF NOT EXISTS YYHList(id TEXT,name TEXT,line INTEGER,enabled BIT,checked BIT)", [])
-        tx.executeSql("SELECT * FROM YYHList", [], function(tx, rs) {
+        tx.executeSql("SELECT * FROM YYHList", [], function(tx, rs) {          
           if(rs.rows.length != raiseConfig.length){
             // 数据库数据不对，重新加载数据库
             tx.executeSql("DELETE FROM YYHList")
             _this.YYHList=[]
             raiseConfig.sort(function(a,b){return a.line - b.line})
                       .map((y)=>{
+                        //所有都勾上
                         Object.assign(y,{checked:true})
+                        //写入数据库，并初始化所有的集资信息
                         _this.addData(db,y.id,y.name,y.line,y.enabled,y.checked)
                       })
           }
@@ -91,10 +96,12 @@ export default {
               let item = rs.rows.item(i)
               item.checked = item.checked=="true"
               item.enabled = item.enabled=="true"
-              _this.YYHList.push(item)
+              _this.YYHList.push(item)              
             }
-          } 
-          _this.initModianDatas(_this)
+            //有一个checked的，all就勾上
+            _this.all = _this.YYHList.filter((x)=>{return x.checked}).length > 0
+            _this.initModianDatas(_this)
+          }
         })
     })
   },
@@ -134,40 +141,36 @@ export default {
         3、执行成功回调函数（可选）
         4、执行失败回调函数（可选）
         */
-       tx.executeSql("SELECT * FROM YYHList WHERE id=?", [id], function(tx, rs) {
-         if(rs.rows.length < 1){
-            tx.executeSql("INSERT INTO YYHList VALUES(?,?,?,?,?)", [id, name, line, enabled,checked], function(tx, rs) {                                      
-                if(enabled){
-                  _this.YYHList.push({
-                    name: name,
-                    id: id,
-                    line: line,
-                    enabled: enabled,
-                    chekced: checked
-                  })
-                }
-              },
-              function(tx, erro) {
-                  alert(erro.source + "::" + erro.message);
-              }              
+        tx.executeSql("SELECT * FROM YYHList WHERE id=?", [id], function(tx, rs) {
+          if(rs.rows.length < 1){
+            tx.executeSql("INSERT INTO YYHList VALUES(?,?,?,?,?)", [id, name, line, enabled, checked], function(tx, rs) {
+              if(enabled){
+                _this.YYHList.push({
+                  name: name,
+                  id: id,
+                  line: line,
+                  enabled: enabled,
+                  checked: checked
+                })
+                _this.getModianData(_this,id)
+              }
+            },
+            function(tx, erro) {
+                alert(erro.source + "::" + erro.message);
+            }
           )}
         })
       })
     },
     exchange (m) {
       let _this = this
-      var db = openDatabase("Mydata", "", "My Database", 1024 * 100);
+      var db = openDatabase("TeamSII", "", "Team SII Database", 1024 * 100);
       db.transaction(function(tx) {
         //console.log(id,!checked)
-        tx.executeSql("UPDATE YYHList SET checked=? WHERE id=?", [!m.checked,m.id], function(tx, rs) {
-                if(!m.checked){
-                  console.log(1)
-                }
-                else{
-                  if(!m.got){
-                    m.got = true
-                    _this.getModianData(_this,m.id)
-                  }
+        tx.executeSql("UPDATE YYHList SET checked=? WHERE id=?", [m.checked,m.id], function(tx, rs) {
+              if(m.checked && !m.got){
+                  m.got = true
+                  _this.getModianData(_this,m.id)
                 }
             })
           },
@@ -187,6 +190,8 @@ export default {
       })
     },
     getModianData (_this,id) {
+      if(_this.modianDatas.filter((x)=>{return x.user_id == id}).length > 0)
+        return
       jQuery.post(`${modianApi}`, { to_user_id: id }, function (response) {
                    let json = JSON.parse(response)
                    //所有项目
