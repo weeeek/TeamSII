f2c6d8eb79ec83fc2940e1046a500c1d84661728
weeeek <407351071@qq.com>
2019-10-21 23:00:06 +0800

    成员信息修改

--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -4,7 +4,7 @@
       <router-link tag="div" class="tab-item" to="/dynamic">
         <span class="tab-link">动态</span>
       </router-link>
-    </keep-alive> -->
+    </keep-alive>-->
     <keep-alive>
       <router-link tag="div" class="tab-item" to="/recommend">
         <span class="tab-link">推荐</span>
@@ -14,7 +14,7 @@
       <router-link tag="div" class="tab-item" to="/video">
         <span class="tab-link">视频</span>
       </router-link>
-    </keep-alive> -->
+    </keep-alive>-->
     <keep-alive>
       <router-link tag="div" class="tab-item" to="/article">
         <span class="tab-link">文章</span>
@@ -44,12 +44,15 @@
       <router-link tag="div" class="tab-item" to="/live">
         <span class="tab-link">直播</span>
       </router-link>
-    </keep-alive> -->
-    <keep-alive>
+    </keep-alive>-->
+    <a class="tab-item" target="_blank" href="http://47.97.248.244/SNH48/tourmap/map.html">
+      <span class="tab-link" >集资</span>
+    </a>
+    <!-- <keep-alive>
       <router-link tag="div" class="tab-item" to="/raise">
         <span class="tab-link">集资</span>
       </router-link>
-    </keep-alive>
+    </keep-alive>-->
     <keep-alive>
       <router-link tag="div" class="tab-item" to="/derivant">
         <span class="tab-link">周边</span>
@@ -64,76 +67,102 @@
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+export default {};
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-@media screen and (min-width 1366px)
-  .tab
-    border-top 1px solid #f2f2f5
-    box-shadow 0 1px 1px rgba(0,0,0,0.15)
-    background-color #FFF
-    display flex
-    height 40px
-    line-height 40px
-    flex-direction row
-    .tab-item
-      flex-grow 1
-      text-align center
-      text-decoration none
-      color #2c3e50
-      .tab-link
-        cursor pointer
-        font-weight bolder
-        padding 0 15px 5px 15px
-    &.router-link-active
-      .tab-link
-        border-bottom 5px solid $color-team-sii
+@media screen and (min-width: 1366px) {
+  .tab {
+    border-top: 1px solid #f2f2f5;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15);
+    background-color: #FFF;
+    display: flex;
+    height: 40px;
+    line-height: 40px;
+    flex-direction: row;
 
-        
-@media screen and (max-width 1366px) and (min-width 768px)
-  .tab
-    border-top 1px solid #f2f2f5
-    box-shadow 0 1px 1px rgba(0,0,0,0.15)
-    background-color #FFF
-    display flex
-    height 32px
-    line-height 32px
-    flex-direction row
-    .tab-item
-      flex-grow 1
-      text-align center
-      text-decoration none
-      color #2c3e50
-      .tab-link
-        cursor pointer
-        font-weight bolder
-        padding 0 6px 2px 6px
-        font-size 16px
-    &.router-link-active
-      .tab-link
-        border-bottom 2px solid $color-team-sii
+    .tab-item {
+      flex-grow: 1;
+      text-align: center;
+      text-decoration: none;
+      color: #2c3e50;
 
-@media screen and (max-width 768px)
-  .tab
-    border-top 1px solid #f2f2f5
-    box-shadow 0 1px 1px rgba(0,0,0,0.15)
-    background-color #FFF
-    display flex
-    height 22px
-    line-height 22px
-    flex-direction row
-    .tab-item
-      flex-grow 1
-      text-align center
-      text-decoration none
-      color #2c3e50
-      .tab-link
-        cursor pointer
-        font-weight bolder
-        padding 0 6px 2px 6px
-        font-size 12px
-    &.router-link-active
-      .tab-link
-        border-bottom 2px solid $color-team-sii
+      .tab-link {
+        cursor: pointer;
+        font-weight: bolder;
+        padding: 0 15px 5px 15px;
+      }
+    }
+
+    &.router-link-active {
+      .tab-link {
+        border-bottom: 5px solid $color-team-sii;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 1366px) and (min-width: 768px) {
+  .tab {
+    border-top: 1px solid #f2f2f5;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15);
+    background-color: #FFF;
+    display: flex;
+    height: 32px;
+    line-height: 32px;
+    flex-direction: row;
+
+    .tab-item {
+      flex-grow: 1;
+      text-align: center;
+      text-decoration: none;
+      color: #2c3e50;
+
+      .tab-link {
+        cursor: pointer;
+        font-weight: bolder;
+        padding: 0 6px 2px 6px;
+        font-size: 16px;
+      }
+    }
+
+    &.router-link-active {
+      .tab-link {
+        border-bottom: 2px solid $color-team-sii;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .tab {
+    border-top: 1px solid #f2f2f5;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15);
+    background-color: #FFF;
+    display: flex;
+    height: 22px;
+    line-height: 22px;
+    flex-direction: row;
+
+    .tab-item {
+      flex-grow: 1;
+      text-align: center;
+      text-decoration: none;
+      color: #2c3e50;
+
+      .tab-link {
+        cursor: pointer;
+        font-weight: bolder;
+        padding: 0 6px 2px 6px;
+        font-size: 12px;
+      }
+    }
+
+    &.router-link-active {
+      .tab-link {
+        border-bottom: 2px solid $color-team-sii;
+      }
+    }
+  }
+}
 </style>
