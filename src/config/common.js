c8256c688c4c162a9288ca0c19704f740c12b925
weeeek <407351071@qq.com>
2020-05-18 15:14:11 +0800

    歌曲url服务器地址写错了。

--- src/config/common.js
+++ src/config/common.js
@@ -50,5 +50,4 @@ export const playMode = {
 }
 
 export const server = 'http://47.97.248.244/json/'
-// export const webProxyServer = 'http://47.97.248.244/WebProxy/'
-export const webProxyServer = 'http://localhost/HttpProxy/'
+export const webProxyServer = 'http://47.97.248.244/WebProxy/'
