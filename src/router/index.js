db4fd09e321136136721ae4898a3de24390ced16
weeeek <407351071@qq.com>
2020-08-03 10:10:17 +0800

    App route history

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,14 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
+function getAbsolutePath() {
+  let path = location.pathname
+  return path.substring(0, path.lastIndexOf('/') + 1)
+}
+
 export default new Router({
+  mode: 'history',
+  base: getAbsolutePath(),
   routes: [
     {
       path: '/',
