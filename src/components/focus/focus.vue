a161750ac762b149edd6cc0eabcbd1f30c7601ac
weeeek <407351071@qq.com>
2020-08-13 19:42:17 +0800

    合影加入直拍栏目

--- src/components/focus/focus.vue
+++ src/components/focus/focus.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="focus-container">
-    <div class="block">
-      <div class="search">
+    <div class="block flex">
+      <div class="search flex-grow">
         <search :placeholder="'搜索关键字（用单英文空格分隔）'" @query="onQueryChange"></search>
       </div>
+      <button class="btn-3d btn-upload float-right" @click="upload">上传</button>
     </div>
+    <h2 style="margin: .5em 0">直拍</h2>
     <div class="project-wrap flex flex-wrap flex-justify-between">
       <a :class="`project-item flex-l-4 flex-m-3 flex-s-2 flex-xs-1 member${i}`" target="_blank" :href="item.url" v-for="(item, i) in focusFilter" :key="i">
         <div class="project-info">
@@ -13,10 +15,20 @@
         </div>
       </a>
     </div>
+    <h2 style="margin: .5em 0">合影</h2>
+    <div class="project-wrap flex flex-wrap flex-justify-between">
+      <a :class="`project-item flex-l-4 flex-m-3 flex-s-2 flex-xs-1 member${i}`" target="_blank" :href="item.img" v-for="(item, i) in photoFilter" :key="i">
+        <div class="project-info">
+          <img v-lazy="item.img" style="object-fit: contain" />
+          <p class="text-ellipsis">{{item.from}}<br />{{item.title}}</p>
+        </div>
+      </a>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import { mapActions } from "vuex";
 import { getFocusData } from 'config/focusData'
 import Search from 'components/plugin/search'
 import { searchMixin } from 'common/js/mixin'
@@ -28,25 +40,40 @@ export default {
   },
   data () {
     return {
-      DataList: []
+      FocusList: [],
+      PhotoList: []
     }
   },
   mounted () {
     getFocusData().then(res => {
       // 全部数据
-      this.DataList = res
+      this.FocusList = res.focus
+      this.PhotoList = res.photo
     })
   },
   methods: {
-
+    upload() {
+      this.setUploadType("Photo");
+      this.triggerUploader();
+    },
+    ...mapActions(["triggerUploader", "setUploadType"]),
   },
   computed: {
     focusFilter () {
       var query = this.query.trim().toLowerCase()
       if (!query) {
-        return this.DataList
+        return this.FocusList
       }
-      return this.DataList.filter((y) => {
+      return this.FocusList.filter((y) => {
+        return y.title.toLowerCase().includes(query) || y.from.toLowerCase().includes(query)
+      })
+    },
+    photoFilter () {
+      var query = this.query.trim().toLowerCase()
+      if (!query) {
+        return this.PhotoList
+      }
+      return this.PhotoList.filter((y) => {
         return y.title.toLowerCase().includes(query) || y.from.toLowerCase().includes(query)
       })
     }
@@ -57,7 +84,9 @@ export default {
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/project"
   @import "~common/stylus/screen"
-
+  @import '~common/stylus/3dButton';
+  .btn-upload
+    margin-left 10px
   #focus-container
     .project-wrap
       column-count unset
@@ -79,4 +108,7 @@ export default {
   @media screen and (max-width 1366px)
     #focus-container
       padding-bottom 10px
+    .btn-upload
+      margin-top 4px
+      margin-bottom 4px
 </style>
